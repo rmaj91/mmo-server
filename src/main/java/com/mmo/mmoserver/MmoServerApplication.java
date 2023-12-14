7f7cc1ae4cfb8a2f40af37a236b3cbfe672f1dc5
rmaj91 <rmaj91@gmail.com>
2023-12-14 20:17:00 +0100

    init

---
+++ src/main/java/com/mmo/mmoserver/MmoServerApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.mmoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmo/mmoserver/MmoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.mmoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
