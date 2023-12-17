71227219f0ad8d7f8d78bebfb49a322c7c1e22c0
rmaj91 <rmaj91@gmail.com>
2023-12-18 00:42:32 +0100

    game engine

--- src/main/java/com/mmo/mmoserver/engine/GameEngine.java
+++ src/main/java/com/mmo/mmoserver/engine/GameEngine.java
@@ -4,24 +4,69 @@ import com.mmo.mmoserver.player.PlayerState;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @Component
 public class GameEngine {
 
+    private Map<String, PlayerState> userToPosition = new HashMap<>();
+    private Map<String, String> userToState = new HashMap<>();
+    private Map<String, Double> userToDirection = new HashMap<>();
+
+    public GameEngine() {
+        start();
+    }
+
+    private void start() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    userToState.forEach((k,v)-> {
+                        if (v.equals("walk")) {
+                            PlayerState position = userToPosition.getOrDefault(k, new PlayerState(10, 0, 10));
+                            Double direction = userToDirection.getOrDefault(k, 0d);
+
+                            //apply magic logic
+                            // Move 1 unit in the direction of rotation
+                            double deltaX = 0.3 * Math.cos(direction);
+                            double deltaZ = 0.3 * Math.sin(direction);
+                            // Calculate the new position
+                            double newX = position.getPx() + deltaX;
+                            double newZ = position.getPz() - deltaZ;
+
+                            PlayerState newState = new PlayerState(newX, position.getPy(), newZ);
+                            log.info("new state: {}, {}, {}", newX, position.getPy(), newZ);
+                            userToPosition.put(k, newState);
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        new Thread(runnable).start();
+    }
 
     public PlayerState getPlayerState(String username) {
-        PlayerState playerState = new PlayerState();
-        playerState.setPx(0);
-        playerState.setPy(0);
-        playerState.setPz(0);
-        return playerState;
+        return userToPosition.getOrDefault(username, new PlayerState(10,0,10));//default position 10,10
     }
 
     public void setPlayerDirection(String username, double rotationY) {
-        log.info("Updating rotationY for player \"{}\", to:\"{}\".", username, rotationY);
+        userToDirection.put(username, rotationY);
+//        log.info("Updating rotationY for player \"{}\", to:\"{}\".", username, rotationY);
     }
 
     public void setPlayerState(String username, String state) {
-        log.info("Updating state for player \"{}\", to: \"{}\".", username, state);
+        userToState.put(username, state);
+//        log.info("Updating state for player \"{}\", to: \"{}\".", username, state);
     }
 }
--- src/main/java/com/mmo/mmoserver/player/PlayerState.java
+++ src/main/java/com/mmo/mmoserver/player/PlayerState.java
@@ -1,12 +1,16 @@
 package com.mmo.mmoserver.player;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerState {
     private double px; // p for position
-    private double pz;
     private double py;
+    private double pz;
 }
