84a5877f28108f7260a960627d8915d57f3cae22
rmaj91 <rmaj91@gmail.com>
2023-12-24 10:27:10 +0100

    cors config

--- src/main/java/com/mmo/mmoserver/config/CorsConfig.java
+++ src/main/java/com/mmo/mmoserver/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://master.d28qw7cijbhxyp.amplifyapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
